7be3a251e31304a2100ccd5a13ed149a7b08c98c
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-04-12 16:12:23 +0100

    passing 5 tests. Just made update controller method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,4 +19,16 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
+  end
+ 
 end
