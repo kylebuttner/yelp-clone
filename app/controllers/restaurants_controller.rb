e506f8428df5f95be44b4d695bcd9c8bc801b30c
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-04-12 15:07:18 +0100

    passing 3 tests.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,16 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
   def new
+    @restaurant = Restaurant.new
   end
 end
