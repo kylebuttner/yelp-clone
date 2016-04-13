d0aeba39627a462f9027152b5d0216cfd9030edf
Gabrielle <gabivk@gmail.com>
2016-04-13 14:39:06 +0100

    added length validation to restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,8 +4,12 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    Restaurant.create(restaurant_params)
-    redirect_to restaurants_path
+    @restaurant = Restaurant.create(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end
   end
 
   def restaurant_params
