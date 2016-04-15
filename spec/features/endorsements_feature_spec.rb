84e8126a9399f2e2cfdfadb5f21a2cd604c31031
Kyle Buttner <kylebuttner@gmail.com>
2016-04-15 13:58:05 +0100

    added ajax request for current endorsement count

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -9,6 +9,6 @@ feature 'endorsing reviews' do
   it 'a user can endorse a review, which updates the review endorsment count', js: true do
     visit '/restaurants'
     click_link 'Endorse'
-    export(page).to have_content('1 endorsement')
+    expect(page).to have_content('1 endorsement')
   end
 end
